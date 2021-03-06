b67ed89ba6a6c7a7407911e654943eac4a66fe15
dawid m. wojciechowski <de.wojciechowski@gmail.com>
2021-03-06 12:01:46 +0100

    fill the TODO gaps

---
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -0,0 +1,18 @@
+import {connect} from 'react-redux';
+import TripListOptions from './TripListOptions';
+import {getAllTags} from '../../../redux/tagsRedux';
+import {getAllFilters, changeSearchPhrase, changeDuration, changeTag} from '../../../redux/filtersRedux';
+
+const mapStateToProps = state => ({
+  tags: getAllTags(state),
+  filters: getAllFilters(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  // (TODO) - add more dispatchers for other filters
+  changeDuration: duration => dispatch(changeDuration(duration)),
+  changeTag: tag => dispatch(changeTag(tag)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
